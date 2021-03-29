c6bcd3e463721de78018e80001d5e04866f2c53c
Sahas <sahas052@gmail.com>
2021-03-30 00:18:48 +0530

    Bootstrapping Order Repository

---
+++ src/main/java/com/cutico/orders/CuticoOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.cutico.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuticoOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuticoOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cutico/orders/CuticoOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cutico.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuticoOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
